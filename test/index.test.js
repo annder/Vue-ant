e7ed87fe990ccfedb278919db25b471492954320
annerx <15623317087@163.com>
2018-09-24 17:58:24 +0800

    fragment

--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,10 @@
 import { Ant } from "./../src/index.version1";
+import {expect} from "chai";
+
+describe("Test object is observerable",function (){
+    it("should change?",function (){}) 
+})
+
 let Object_ = { val: 1 }
 const Ant_Vue = new Ant(Object_, function (obj) {
     console.log(`New value is ----> ${obj}`);
