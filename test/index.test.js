a8c63317e5cc8513bc7cdcba4b89199b3914b7e5
annerx <15623317087@163.com>
2018-09-24 18:00:48 +0800

    push test fragment

--- test/index.test.js
+++ test/index.test.js
@@ -2,12 +2,13 @@ import { Ant } from "./../src/index.version1";
 import {expect} from "chai";
 
 describe("Test object is observerable",function (){
-    it("should change?",function (){}) 
+    it("should change?",function (){
+        let Object_ = { val: 1 }
+        const Ant_Vue = new Ant(Object_, function (obj) {
+            console.log(`New value is ----> ${obj}`);
+        });        
+        Object_.val = 1;
+        expect(Object_.val).equal(1);
+    })
 })
 
-let Object_ = { val: 1 }
-const Ant_Vue = new Ant(Object_, function (obj) {
-    console.log(`New value is ----> ${obj}`);
-});
-Object_.val = 213;
-Object_.val = 312;
